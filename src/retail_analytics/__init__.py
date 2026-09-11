df814fb6f77a62fa6eb0c96a489104caaca29bf6
vivekkattri <wickedvivek1816@gmail.com>
2026-09-11 05:15:23 +0200

    add notebook content

---
+++ src/retail_analytics/__init__.py
@@ -0,0 +1,7 @@
+"""Retail sales and customer analytics.
+
+Cleaning, exploratory analysis, RFM segmentation, and cohort
+retention for the UCI Online Retail II dataset.
+"""
+
+__version__ = "0.1.0"
